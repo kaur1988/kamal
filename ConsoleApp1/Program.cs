684c86986389c71c74f4cd9577f5c26f61cf865e
kaur1988 <you@example.com>
2019-05-16 19:15:11 -0400

    Initial Commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter your code:");
+            int code = int.Parse(Console.ReadLine());
+            int start = 0;
+            Console.WriteLine("even numbers are:");
+            while(start<=code)
+            {
+                Console.Write(start +"  ");
+                Console.WriteLine();
+                Console.WriteLine(); 
+                start = start + 2;
+                public int MyProperty { get; set; }
+        public Program()
+        {
+            for (int i = 0; i < length; i++)
+                Console.WriteLine();
+            {
+
+            }
+        }
+    }
+        }
+    }
+}
